cdfbb1c0bda3dfe795217b87f483f2356f67f355
Julien Ducro <julien.ducro@deskpro.com>
2023-06-30 12:32:15 +0100

    Upgrade node

--- postcss.config.js
+++ postcss.config.js
@@ -1,53 +1,55 @@
-var postcss = require('postcss');
 var contrast = require('contrast');
 
-const contrastPlugin = postcss.plugin('postcss-contrast', function(opts) {
+const contrastPlugin = (opts = {}) => {
   opts = opts || {};
   var dark = opts.dark;
   var light = opts.light;
 
-  return function(css) {
-    css.walkDecls(function(decl) {
-      if (!decl.value || decl.value.indexOf('contrast(') === -1) {
-        return;
-      }
+  return {
+    postcssPlugin: 'postcss-contrast',
+    Once (root, { result }) {
+      root.walkDecls(function(decl) {
+        if (!decl.value || decl.value.indexOf('contrast(') === -1) {
+          return;
+        }
 
-      var index = decl.value.indexOf('(');
-      var last = decl.value.indexOf(')');
-      var value = decl.value.slice(++index, last);
-      var black = '#000';
-      var white = '#fff';
+        var index = decl.value.indexOf('(');
+        var last = decl.value.indexOf(')');
+        var value = decl.value.slice(++index, last);
+        var black = '#000';
+        var white = '#fff';
 
 
-      return new Promise(function(resolve) {
-        if (contrast(value) === 'light') {
-          if (!dark) {
-            decl.value = black;
+        return new Promise(function(resolve) {
+          if (contrast(value) === 'light') {
+            if (!dark) {
+              decl.value = black;
+            }
+            else {
+              decl.value = opts.dark;
+            }
+            resolve();
           }
           else {
-            decl.value = opts.dark;
+            if (!light) {
+              decl.value = white;
+            }
+            else {
+              decl.value = opts.light;
+            }
+            resolve();
           }
-          resolve();
-        }
-        else {
-          if (!light) {
-            decl.value = white;
-          }
-          else {
-            decl.value = opts.light;
-          }
-          resolve();
-        }
+        });
       });
-    });
+    }
   };
-});
+};
+contrastPlugin.postcss = true;
 
-module.exports = ctx => ({
-  map: ctx.options.map,
+const config = {
   plugins: [
-    require("stylelint"),
-    require('postcss-import')({ root: ctx.file.dirname }),
+    require('stylelint')({}),
+    require('postcss-import'),
     require('postcss-autoreset')({
       reset: {
         'animation': 'none 0s ease 0s 1 normal none running',
@@ -141,4 +143,6 @@ module.exports = ctx => ({
     contrastPlugin(),
     require("postcss-reporter")({ clearReportedMessages: true })
   ]
-});
+}
+
+module.exports = config
