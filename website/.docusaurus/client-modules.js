668b73f0df062f859a3c75ebb859a59e44543044
Julien Ducro <julien.ducro@deskpro.com>
2024-02-21 13:32:11 +0100

    Upgrade docusaurus to 3

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/jducro/Repositories/messenger-style/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/jducro/Repositories/messenger-style/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/jducro/Repositories/messenger-style/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/jducro/Repositories/messenger-style/website/src/css/customTheme.css'),
-];
